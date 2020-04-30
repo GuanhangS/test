399d92a0252e00ebf2ede43bde21cf85860ac3e2
Guanhang <guanhangshi@gmail.com>
2020-04-30 15:18:51 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var controller = require('./controller/controller');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+controller(app);
+
+app.listen(process.env.PORT || 3000);
+
---
+++ controller/controller.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://Todo1234:Todo1234@cluster0-jhupg.mongodb.net/test?retryWrites=true&w=majority')
+
+
+var todoSchema = new mongoose.Schema({
+    item:String
+});
+
+
+var Todo = mongoose.model('Todo',todoSchema);
+
+
+var bodyParser = require('body-parser');
+// parse data
+var urlencodeParser = bodyParser.urlencoded({extended:false});
+
+// var data = [
+//     {item:"hello world!"},
+//     {item:"hello world!!"},
+//     {item:"hello world!!!"}
+// ];
+
+module.exports = function (app) {
+    // get
+    app.get('/todo',function (req,res) {
+        Todo.find({},function (err,data) {
+            if (err) throw err;
+
+            res.render('todo',{todos:data});
+        })
+    });
+
+    // post
+    app.post('/todo',urlencodeParser,function (req,res) {
+        Todo(req.body).save(function (err,data) {
+            if (err) throw err;
+            res.json(data);
+        })
+    });
+
+    // delete
+    app.delete('/todo/:item',function (req,res) {
+        // console.log(req.params.item);
+        Todo.find({item:req.params.item}).remove(function (err,data) {
+            if (err) throw err;
+            res.json(data);
+        })
+        // data = data.filter(function (todo) {
+        //     return req.params.item !== todo.item;
+        // });
+        //
+        // res.json(data);
+    });
+}
